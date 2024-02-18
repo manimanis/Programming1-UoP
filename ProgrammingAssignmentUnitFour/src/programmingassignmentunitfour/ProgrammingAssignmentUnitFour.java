5d3122d86871e392f21a0a3a9238fbe7bbd3d9f4
Mohamed Anis MANI <manimohamed@gmail.com>
2024-02-18 11:54:19 +0100

    Assignment Four

--- ProgrammingAssignmentUnitFour/src/programmingassignmentunitfour/ProgrammingAssignmentUnitFour.java
+++ ProgrammingAssignmentUnitFour/src/programmingassignmentunitfour/ProgrammingAssignmentUnitFour.java
@@ -1,9 +1,9 @@
 package programmingassignmentunitfour;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -16,31 +16,29 @@ public class ProgrammingAssignmentUnitFour {
      */
     public static void main(String[] args) {
         // Stock prices for the period of Jan, 29 to Feb, 9
-        double[] stockPrices = new double[]{58.32, 58.11, 58.60, 57.54, 56.92,
-            56.24, 55.83, 56.74, 56.7, 56.88};
+        double[] stockPrices = new double[] { 58.32, 58.11, 58.60, 57.54, 56.92,
+                56.24, 55.83, 56.74, 56.7, 56.88 };
         List<double[]> arrStockPrices = Arrays.asList(stockPrices);
 
-        double average = calculateAveragePrice(stockPrices);
-        double std = calculateStandardDeviation(stockPrices);
         double max = findMaximumPrice(stockPrices);
         double min = findMinimumPrice(stockPrices);
-        double[] quart = findQuartiles(stockPrices);
-        double interQuart = quart[3] - quart[1];
 
-        
         displayStocks(stockPrices);
         displayGraphic(stockPrices, min, max);
+        displaySummary(stockPrices);
 
-        System.out.println("\n--- Stock Prices Summary ---\n");
-        System.out.printf("Average: %5.2f\n", average);
-        System.out.printf("Standard Deviation: %5.2f\n", std);
-        System.out.printf("Minimum: %5.2f\n", min);
-        System.out.printf("Q1: %5.2f\n", quart[1]);
-        System.out.printf("Median: %5.2f\n", quart[2]);
-        System.out.printf("Q3: %5.2f\n", quart[3]);
-        System.out.printf("Maximum: %5.2f\n", max);
-        System.out.printf("Q3 - Q1: %5.2f\n", interQuart);
+        stockPrices = generateRandomStock(15, 40, 80);
+        max = findMaximumPrice(stockPrices);
+        min = findMinimumPrice(stockPrices);
+        displayStocks(stockPrices);
+        displayGraphic(stockPrices, min, max);
+        displaySummary(stockPrices);
 
+        double[] bins = createBins(min, max, 5);
+        int[] freq = calculateOccurences(stockPrices, bins);
+        for (int i = 0; i < bins.length - 1; i++) {
+            System.out.println("[" + bins[i] + ", " + bins[i + 1] + "] " + freq[i]);
+        }
     }
 
     /**
@@ -85,7 +83,7 @@ public class ProgrammingAssignmentUnitFour {
      *
      * @param stockPrices An array of stock prices.
      * @param targetPrice target price
-     * @param delta target price range
+     * @param delta       target price range
      * @return the number of occurrence
      */
     public static int countOccurences(double[] stockPrices,
@@ -207,18 +205,97 @@ public class ProgrammingAssignmentUnitFour {
         double step = (max - min) / 50;
         for (int i = 0; i < stockPrices.length; i++) {
             if (i == 0) {
-            System.out.printf("Day | $%5.2f", min);
+                System.out.printf("Day | $%5.2f", min - step);
                 displayChar(' ', 44);
-            System.out.printf("| $%5.2f\n", max);
+                System.out.printf("| $%5.2f\n", max + step);
                 displayChar('─', 65);
                 System.out.println();
             }
-            System.out.printf("%3d |", i+1);
-            displayChar('▓', (int)Math.ceil((stockPrices[i] - min + step) / step));
+            System.out.printf("%3d |", i + 1);
+            displayChar('▓', (int) Math.ceil((stockPrices[i] - min + step) / step));
             System.out.printf(" $%5.2f\n", stockPrices[i]);
         }
     }
-    
+
+    /**
+     * 
+     * @param stockPrices
+     */
+    public static void displaySummary(double[] stockPrices) {
+        double average = calculateAveragePrice(stockPrices);
+        double std = calculateStandardDeviation(stockPrices);
+        double max = findMaximumPrice(stockPrices);
+        double min = findMinimumPrice(stockPrices);
+        double[] quart = findQuartiles(stockPrices);
+        double interQuart = quart[3] - quart[1];
+
+        System.out.println("\n--- Stock Prices Summary ---\n");
+        System.out.printf("Average: %5.2f\n", average);
+        System.out.printf("Standard Deviation: %5.2f\n", std);
+        System.out.printf("Minimum: %5.2f\n", min);
+        System.out.printf("Q1: %5.2f\n", quart[1]);
+        System.out.printf("Median: %5.2f\n", quart[2]);
+        System.out.printf("Q3: %5.2f\n", quart[3]);
+        System.out.printf("Maximum: %5.2f\n", max);
+        System.out.printf("Q3 - Q1: %5.2f\n", interQuart);
+    }
+
+    /**
+     * 
+     * @param count
+     * @param min
+     * @param max
+     * @return
+     */
+    public static double[] generateRandomStock(int count, double min, double max) {
+        double[] items = new double[count];
+        double nextDown = Math.nextDown(1.0);
+        for (int i = 0; i < count; i++) {
+            items[i] = Math.floor(((Math.random() / nextDown) * (max - min) + min) * 100) / 100;
+        }
+        return items;
+    }
+
+    /**
+     * 
+     * @param min
+     * @param max
+     * @param binsCount
+     * @return
+     */
+    public static double[] createBins(double min, double max, int binsCount) {
+        double[] bins = new double[binsCount + 1];
+        double step = (max - min) / binsCount;
+        bins[0] = min;
+        for (int i = 1; i <= binsCount; i++) {
+            bins[i] = bins[i - 1] + step;
+        }
+        return bins;
+    }
+
+    public static int[] calculateOccurences(double[] stockPrices, double[] bins) {
+        int[] freq = new int[bins.length];
+        for (int i = 0; i < stockPrices.length; i++) {
+            int idx = indexOf(bins, stockPrices[i]);
+            System.out.println(stockPrices[i] + " " + idx);
+            freq[idx]++;
+        }
+        return freq;
+    }
+
+    public static int indexOf(double[] arr, double value) {
+        int d = 0, f = arr.length - 1, mid = 0;
+        while (f > d) {
+            mid = (d + f) / 2;
+            if (value >= arr[mid]) {
+                d = mid;
+            } else if (value < arr[mid]) {
+                f = mid - 1;
+            }
+        }
+        return mid;
+    }
+
     private static void displayChar(char c, int count) {
         for (int i = 0; i < count; i++) {
             System.out.print(c);
