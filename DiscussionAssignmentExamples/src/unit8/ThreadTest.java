3689b2ee993da58c360bafb2e0cbd8b9721f2044
Mohamed Anis MANI <manimohamed@gmail.com>
2024-03-21 14:17:40 +0100

    Assignment Eight

--- DiscussionAssignmentExamples/src/unit8/ThreadTest.java
+++ DiscussionAssignmentExamples/src/unit8/ThreadTest.java
@@ -1,4 +1,3 @@
-
 package unit8;
 
 import java.util.ArrayList;
@@ -13,79 +12,109 @@ import java.util.logging.Logger;
  * @author manianis
  */
 public class ThreadTest {
+
+    public static void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ThreadTest.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void join(Thread thread) {
+        try {
+            thread.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ThreadTest.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException {
         ArrayList<Integer> arr = new ArrayList<>();
-        ProducerThread pt = new ProducerThread(arr);
+
+        // Creates producer and consumer threads
+        ProducerThread[] pt = new ProducerThread[3];
+        for (int i = 0; i < pt.length; i++) {
+            pt[i] = new ProducerThread(arr); pt[i].start();
+        }
         ConsumerThread[] ct = new ConsumerThread[10];
         for (int i = 0; i < ct.length; i++) {
-            ct[i] = new ConsumerThread(arr, i);
-            ct[i].start();
+            ct[i] = new ConsumerThread(arr, i); ct[i].start();
         }
-        pt.start();
-        Thread monitor = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Monitor thread started!");
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(ThreadTest.class.getName())
-                            .log(Level.SEVERE, null, ex);
-                }
-                pt.stopThread();
-                for (int i = 0; i < ct.length; i++) {
-                    ct[i].stopThread();
-                }
-                try {
-                    pt.join();
-                    for (int i = 0; i < ct.length; i++) {
-                        ct[i].join();
-                    }
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(ThreadTest.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                System.out.println("Monitor thread ended!");
-            }
+
+        // Creates monitor thread
+        Thread monitor = new Thread(() -> {
+            System.out.println("Monitor thread started!");
+            // Give some time to producer and consumers
+            // to do their jobs
+            sleep(1000);
+
+            // Stop all threads
+            for (ProducerThread thread : pt) thread.stopThread();
+            for (ConsumerThread thread : ct) thread.stopThread();
+
+            // Wait for the threads to return
+            for (ProducerThread thread : pt) join(thread);
+            for (ConsumerThread thread : ct) join(thread);
+            System.out.println("Monitor thread ended!");
         });
         monitor.start();
         monitor.join();
-        System.out.print("Collection content: ");
-        System.out.println(arr);
+
+        System.out.println("Collection contains " + arr.size() + " items.");
+        for (int i = 0; i < 10; i++) {
+            final Integer x = i;
+            System.out.print(i + ": ");
+            int count = (int) arr.stream()
+                    .filter((val) -> val % 10 == x)
+                    .peek((val) -> System.out.print(val + ", "))
+                    .count();
+            System.out.println(" -> " + count);
+        }
     }
 }
 
 class ProducerThread extends Thread {
-    private Collection<Integer> collection;
+
     private static Random rand = new Random();
+
+    private final Collection<Integer> collection;
     private boolean running = true;
-    
+    private int count = 0;
+
     public ProducerThread(Collection<Integer> collection) {
         this.collection = collection;
     }
-    
+
     public synchronized void stopThread() {
         running = false;
     }
 
+    public void add(Integer val) {
+        synchronized (collection) {
+            collection.add(val);
+        }
+    }
+
     @Override
     public void run() {
-        System.out.println("Producer thread started!");
+        System.out.println("Producer started!");
         while (running) {
-            collection.add(rand.nextInt(1, 99));
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(ProducerThread.class.getName())
-                        .log(Level.SEVERE, null, ex);
-            }
+            add(rand.nextInt(1, 99));
+            count++;
+            ThreadTest.sleep(10);
         }
-        System.out.println("Producer thread ended!");
+        System.out.println("Producer produced " + count + " items.");
     }
 }
 
 class ConsumerThread extends Thread {
-    private List<Integer> collection;
-    private int remainder;
+
+    private static final int MIN_ITEMS = 10;
+    private final List<Integer> collection;
+    private final int remainder;
     private boolean running = true;
     private int count = 0;
 
@@ -93,30 +122,60 @@ class ConsumerThread extends Thread {
         this.collection = collection;
         this.remainder = remainder;
     }
-    
+
     public synchronized void stopThread() {
         running = false;
     }
 
-    @Override
-    public void run() {
-        System.out.println("Consumer thread " + remainder + " started!");
-        while (running) {
-            for (int i = collection.size() - 1; i >= 0; i--) {
-                int val = collection.get(i);
+    public boolean hasEnoughItems() {
+        int nb = 0;
+        synchronized (collection) {
+            for (int i = 0; i < collection.size(); i++) {
+                Integer val = collection.get(i);
                 if (val % 10 == remainder) {
-                    System.out.println(val + " % 10 == " + remainder);
-                    collection.remove(i);
-                    count++;
+                    nb++;
                 }
             }
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(ProducerThread.class.getName())
-                        .log(Level.SEVERE, null, ex);
+        }
+        return nb >= MIN_ITEMS;
+    }
+
+    public void removeItems() {
+        synchronized (collection) {
+            int nb = 0, i = 0;
+            while (i < collection.size() && nb < MIN_ITEMS) {
+                int val = collection.get(i);
+                if (val % 10 == remainder) {
+                    if (nb == 0) {
+                        System.out.print(remainder + ": ");
+                    }
+                    System.out.print(val + ", ");
+                    collection.remove(i);
+                    nb++;
+                    count++;
+                } else {
+                    i++;
+                }
+            }
+            if (nb != 0) {
+                System.out.println();
             }
         }
-        System.out.println("Consumer thread consumed " + count);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Consumer " + remainder + " started!");
+        while (running) {
+            if (hasEnoughItems()) {
+                removeItems();
+            }
+            ThreadTest.sleep(10);
+        }
+        if (hasEnoughItems()) {
+            removeItems();
+        }
+        System.out.println("Consumer " + remainder
+                + " consumed " + count + " items.");
     }
 }
